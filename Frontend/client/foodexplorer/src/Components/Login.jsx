3a3a12b2383ebb414d3fd10e682c8b7f51d344a7
Shaswath <shaswath.g@kalvium.community>
2024-03-08 12:25:43 +0530

    patch1

--- Frontend/client/foodexplorer/src/Components/Login.jsx
+++ Frontend/client/foodexplorer/src/Components/Login.jsx
@@ -19,7 +19,7 @@ function LoginPage() {
     const handleSubmit = async (e) => {
         e.preventDefault();
         try {
-            const response = await fetch('http://localhost:3000/login', {
+            const response = await fetch('https://foodexplorer-iqox.onrender.com/login', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json'
